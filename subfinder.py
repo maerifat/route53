7d11f8a3e1535171395e0ca6f9f5898c3357f161
maerifat <76250027+maerifat@users.noreply.github.com>
2023-06-03 09:22:20 +0530

    Update subfinder.py
    using set for unique subdomains

--- subfinder.py
+++ subfinder.py
@@ -16,15 +16,16 @@ session = boto3.Session(profile_name='dns')
 route53 = session.client('route53')
 
 
+
 def get_subdomains(zone_id):
-    subdomains = []
+    subdomains = set()
     try:
         response = route53.list_resource_record_sets(
             HostedZoneId=zone_id,
         )
         for record in response['ResourceRecordSets']:
             if record['Type'] not in ['SOA', 'NS', 'MX', 'TXT'] and not record['Name'].startswith('_') and '_domainkey' not in record['Name']:
-                subdomains.append(record['Name'].rstrip('.'))
+                subdomains.add(record['Name'].rstrip('.'))
     except Exception as e:
         print(f"Failed to get subdomains for zone {zone_id}: {e}")
     with open("subs.txt","a") as file:
